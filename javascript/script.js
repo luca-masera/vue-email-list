3280cb6b1eb08fb99db2471b4fff90c8a50af734
luca-masera <lucamasera1995@gmail.com>
2023-11-07 15:46:16 +0100

    ciclo in funzione per creare 10 email in console

--- javascript/script.js
+++ javascript/script.js
@@ -13,16 +13,21 @@ createApp({
     methods : {
         
         getEmails(){
-        
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp)=>{
-            console.log(resp.data.response)
-            this.email = resp.data.response;                            
-            })
+            
+            for(let i = 0; i < 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res)=>{
+                console.log(res.data.response)
+                this.email = res.data.response;
+                            
+                                  
+                })
+            }
         }
     },
     
     created(){
         this.getEmails()
+        
     }
    
 }).mount('#app')
