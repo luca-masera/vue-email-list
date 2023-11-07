f6c265e1134efe47ad9d33c33ad16a43d521df6e
luca-masera <lucamasera1995@gmail.com>
2023-11-07 15:54:29 +0100

    ciclo v-for in html per stampare in pagina

--- javascript/script.js
+++ javascript/script.js
@@ -16,10 +16,11 @@ createApp({
             
             for(let i = 0; i < 10; i++){
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res)=>{
-                console.log(res.data.response)
-                this.email = res.data.response;
-                            
-                                  
+                //console.log(res)   
+                console.log(res.data.response)                
+                this.emails.push(res.data.response)
+                
+                                
                 })
             }
         }
